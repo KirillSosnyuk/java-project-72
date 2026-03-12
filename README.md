8ad031ebfd2fcca2c9386cfb77c0542a47b4f119
Kirill <kirill-sosnyuk@yandex.ru>
2026-03-12 14:32:09 +0000

    sonar connected

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/KirillSosnyuk/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/KirillSosnyuk/java-project-72/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=KirillSosnyuk_java-project-72&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=KirillSosnyuk_java-project-72)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=KirillSosnyuk_java-project-72&metric=coverage)](https://sonarcloud.io/summary/new_code?id=KirillSosnyuk_java-project-72)
+[![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=KirillSosnyuk_java-project-72&metric=sqale_rating)](https://sonarcloud.io/summary/new_code?id=KirillSosnyuk_java-project-72)
---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("application")
+    id("com.github.ben-manes.versions") version "0.47.0"
+    id("org.sonarqube") version "7.2.3.7755"
+    checkstyle
+    jacoco
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "KirillSosnyuk_java-project-72")
+        property("sonar.organization", "kirillsosnyuk")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml")
+                .get()
+                .asFile
+                .absolutePath
+        )
+
+        // JUnit XML report
+        property(
+            "sonar.junit.reportPaths",
+            layout.buildDirectory.dir("test-results/test")
+                .get()
+                .asFile
+                .absolutePath
+        )
+    }
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    reports {
+        junitXml.required.set(true)
+        junitXml.outputLocation.set(layout.buildDirectory.dir("test-results/test"))
+        html.required.set(true)
+    }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml"))
+        html.required.set(true)
+    }
+}
